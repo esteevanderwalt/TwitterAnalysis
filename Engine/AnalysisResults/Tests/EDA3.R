c95a36d173adf37d0c655ab44eb26f7ea9116dc8
Estee van der Walt <estee.vanderwalt@gmail.com>
2017-09-29 06:40:29 +0200

    Commit latest changes

--- Engine/AnalysisResults/Tests/EDA3.R
+++ Engine/AnalysisResults/Tests/EDA3.R
@@ -17,7 +17,7 @@ myconn<-odbcConnect("SAPHANA", uid="SYSTEM", pwd="oEqm66jccx", believeNRows=FALS
 
 #' ###Load data
 #+ get_data
-tl <- system.time(data.original <- sqlQuery(myconn, "SELECT ID, NAME, SCREENNAME, CREATED, ORIGINAL_PROFILE_IMAGE, PROFILE_IMAGE, BACKGROUND_IMAGE, LAST_TWEET, DESCRIPTION, LOCATION, LANGUAGE, FRIENDS_COUNT, FOLLOWERS_COUNT, STATUS_COUNT, LISTED_COUNT, TIMEZONE, UTC_OFFSET, GEO_ENABLED, LATITUDE, LONGITUDE, IS_DEFAULT_PROFILE, IS_DEFAULT_PROFILE_IMAGE, IS_BACKGROUND_IMAGE_USED, PROFILE_TEXT_COLOR, PROFILE_BG_COLOR, CLASS from twitter.zz_rfull_set") )
+tl <- system.time(data.original <- sqlQuery(myconn, "SELECT ID, NAME, SCREENNAME, CREATED, ORIGINAL_PROFILE_IMAGE, PROFILE_IMAGE, BACKGROUND_IMAGE, LAST_TWEET, DESCRIPTION, LOCATION, LANGUAGE, FRIENDS_COUNT, FOLLOWERS_COUNT, STATUS_COUNT, LISTED_COUNT, TIMEZONE, UTC_OFFSET, GEO_ENABLED, LATITUDE, LONGITUDE, IS_DEFAULT_PROFILE, IS_DEFAULT_PROFILE_IMAGE, IS_BACKGROUND_IMAGE_USED, PROFILE_TEXT_COLOR, PROFILE_BG_COLOR, CLASS from twitter.zz_full_set") )
 
 close(myconn)
 
@@ -30,8 +30,15 @@ data.full <- data.original
 nzv <- nearZeroVar(data.full, saveMetrics= TRUE)
 nzv[nzv$nzv,]
 
-#rapply(data.full,function(x)length(unique(x)))
-#rapply(d,function(x)sum(is.na(x)))
+rapply(data.full,function(x)length(unique(x)))
+rapply(data.original,function(x)sum(is.na(x)))
+#all orig
+i1 <- data.original[data.original$CLASS=='deceptive',]
+rapply(i1,function(x)length(unique(x)))
+#all injected
+i2 <- data.original[data.original$CLASS=='trustworthy',]
+rapply(i2,function(x)length(unique(x)))
+
 
 data.full <- data.full[ , -which(names(data.full) %in% c("ID","NAME","SCREENNAME","DESCRIPTION","IS_CELEBRITY","LAST_TWEET"))]
 
@@ -65,6 +72,74 @@ data.clean <- cleanup.Twitter(data.full)
 data.clean <- data.clean[ , -which(names(data.clean) %in% c("BACKGROUND_IMAGE", "IS_BACKGROUND_IMAGE_USED", "PROFILE_TEXT_COLOR", "PROFILE_BG_COLOR"))]
 data.clean <- data.clean[ , -which(names(data.clean) %in% c("CREATED", "ORIGINAL_PROFILE_IMAGE"))]
 data.clean <- data.clean[ , -which(names(data.clean) %in% c("GEO_ENABLED", "IS_DEFAULT_PROFILE", "IS_DEFAULT_PROFILE_IMAGE"))]
+
+#get bin plots
+
+t <- data.clean[data.clean$FOLLOWERS_COUNT < 100,]
+
+p <- ggplot(t, aes(x=FOLLOWERS_COUNT)) + 
+  geom_bar(stat="count") + 
+  theme_bw() +
+  science_theme +
+  #scale_x_continuous(name="# of friends", label=comma) +
+  scale_y_continuous(name="# of accounts per followers_count group", label=comma) +
+  labs(x = "Bin #") 
+print(p)
+
+setwd("~/Projects/RStudio/TwitterAnalysis/Engine/AnalysisResults/Results")
+pdf("binning_followers.pdf", width = 7, height = 5)
+p
+dev.off()
+
+M <- cor(data.clean[,1:11])
+
+cor.mtest <- function(mat, ...) {
+  mat <- as.matrix(mat)
+  n <- ncol(mat)
+  p.mat<- matrix(NA, n, n)
+  diag(p.mat) <- 0
+  for (i in 1:(n - 1)) {
+    for (j in (i + 1):n) {
+      tmp <- cor.test(mat[, i], mat[, j], ...)
+      p.mat[i, j] <- p.mat[j, i] <- tmp$p.value
+    }
+  }
+  colnames(p.mat) <- rownames(p.mat) <- colnames(mat)
+  p.mat
+}
+# matrix of the p-value of the correlation
+p.mat <- cor.mtest(data.clean[,1:11])
+
+library(scales)
+library(corrplot)
+
+corrplot(M, method="number")
+# alternative to use a corrplot
+corrplot(M, type="upper", order="hclust", 
+         p.mat = p.mat, sig.level = 0.00005,
+         tl.col="black")
+print(p)
+
+mosthighlycorrelated <- function(mydataframe,numtoreport)
+{
+  # find the correlations
+  cormatrix <- cor(mydataframe)
+  # set the correlations on the diagonal or lower triangle to zero,
+  # so they will not be reported as the highest ones:
+  diag(cormatrix) <- 0
+  cormatrix[lower.tri(cormatrix)] <- 0
+  # flatten the matrix into a dataframe for easy sorting
+  fm <- as.data.frame(as.table(cormatrix))
+  # assign human-friendly names
+  names(fm) <- c("First.Variable", "Second.Variable","Correlation")
+  # sort and print the top n correlations
+  head(fm[order(abs(fm$Correlation),decreasing=T),],n=numtoreport)
+}
+
+mosthighlycorrelated(data.clean[,1:11], 10)
+
+nzv <- nearZeroVar(data.clean, saveMetrics= TRUE)
+nzv[nzv$nzv,]
 
 #remove unique values
 #data.clean <- data.clean[ , -which(names(data.clean) %in% c("LOCATION", "LONGITUDE", "LATITUDE"))]
@@ -142,13 +217,13 @@ p <- ggplot(d, aes(x=CLASS, y=value, fill=CLASS)) +
   coord_flip() +
   theme_bw() +
   science_theme +
-  labs(x = "CLASS", y = "Metadata") +
+  labs(x = "Class", y = "Metadata") +
   geom_boxplot(notch=FALSE)
 print(p)
 
 setwd("~/Projects/RStudio/TwitterAnalysis/Engine/AnalysisResults/Results")
 
-png(filename = "rmetadata_distribution.png", width = 500, height = 250)
+pdf("metadata_distribution.pdf", width = 7, height = 6)
 p
 dev.off()
 
@@ -160,7 +235,7 @@ graph1 <- function(data, a) {
     geom_density(size=1.0) 
   #print(p)
   
-  png(filename = paste("rmeta_",a,"_v1.png",sep=""), width = 500, height = 250)
+  pdf(paste("meta_",a,"_v2.png",sep=""), width = 6, height = 4)
   print(p)
   dev.off()
 }
